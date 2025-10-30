5b40c0c4341af42838d7c07cae5379ab96e392c3
amisticaz <kethelenfernandes13@gmail.com>
2025-10-30 12:35:22 -0300

    Fase 04

---
+++ Introducao_as_matrizes/indice_de_matriz_01.js
@@ -0,0 +1,7 @@
+let notas = [10, 5, 13];
+
+// Acessa o primeiro elemento da matriz (índice 0)
+let primeiraNota = notas[0];
+
+// Exibe no console o valor da variável
+console.log(primeiraNota);
---
+++ Introducao_as_matrizes/indice_de_matriz_02.js
@@ -0,0 +1,4 @@
+let notas = [10, 5, 13];
+
+let segundaNota = notas[1]; // pega o segundo elemento da array
+console.log(segundaNota);    // imprime no console o valor 5
